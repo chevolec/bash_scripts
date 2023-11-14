2445b32c37619d9a970d4f2f05731d2d75893f86
chevolec <chevolec@gmail.com>
2023-11-14 01:08:06 -0600

    added Name with number and date, not working adding a date

---
+++ add2calibre/NameWithNumberAndDate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Ruta de la carpeta que contiene los archivos rar
+ruta_carpeta="/mnt/libros/temp/"
+
+# Cambiar al directorio que contiene los archivos rar
+cd "$ruta_carpeta"
+
+# Iterar sobre los archivos rar en la carpeta
+for archivo_rar in *.rar; do
+    # Obtener el nombre de la serie (nombre de la carpeta sin espacios)
+    nombre_serie=$(unrar lb "$archivo_rar" | head -n 1 | sed 's/\/.*//g')
+    nombre_archivo=$(unrar lb "$archivo_rar" | head -n 1 | sed 's/\// - /g' )
+
+    # Crear la carpeta para extraer los archivos rar
+    carpeta_destino="/mnt/libros/temp3/$nombre_serie"
+    mkdir -p "$carpeta_destino"
+
+    # Extraer los archivos rar
+    unrar e -o- "$archivo_rar" "$carpeta_destino"
+
+    # Iterar sobre los archivos extraídos
+    for archivo_libro in "$carpeta_destino"/*; do
+        # Obtener el número del ejemplar (nombre del archivo sin extensión)
+        numero_ejemplar=$(basename "$archivo_libro" | cut -f1 -d'.')
+        # Crear el título del libro concatenando el nombre de la serie y el número del ejemplar
+        titulo_libro="$numero_ejemplar"
+        nombre_libro=$(echo "$titulo_libro" | sed 's/\([^(]*\).*/\1/')
+        numero_libro=$(echo "$titulo_libro" | sed 's/.* \([0-9]*\) .*/\1/')
+        numero_libro=$(echo "$numero_libro" | sed 's/[^0-9]*//g')
+        
+        fecha_raw=$(echo "$libro" | sed -n 's/.* \([0-9]\{2\}[a-zA-Z]\+[0-9]\{4\}\).*/\1/p')
+        fecha=$(date -d "$fecha_raw" +"%Y-%m-%d")
+
+
+        echo $titulo_libro
+        # Agregar el libro a Calibre
+        #calibredb add --title "$titulo_libro" --authors "Nombre del Autor" "$archivo_libro"
+        calibredb add --title "$titulo_libro" --series "$nombre_serie" --series-index "$numero_libro" --tag "Historietas, Retro Pdf" "$archivo_libro"
+    done
+done
+
